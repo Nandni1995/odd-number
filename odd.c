162dd590b83ba21f9400afb4f31e606989b56d5f
Dersana <33249003+Dersana@users.noreply.github.com>
2018-10-31 15:53:57 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,10 +1,11 @@
+//printing odd numbers in a range
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
 int a,b,i;
 clrscr();
-printf("enter the two numbers");
+printf("enter the two limits");
 scanf("%d%d",&a,&b);
 for(i=a;i<=b;i++)
 {
